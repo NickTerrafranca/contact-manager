c1611a70c1ed89b598ce94c76323bcaebcf57592
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-06-09 09:47:16 -0400

    Added rout to show page.

--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
+require 'pry'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 require_relative 'models/contact'
 
 get '/' do
-  @contacts = Contact.all
+  @contacts= Contact.all
   erb :index
+end
+
+get '/contact/:id' do
+  @contact = Contact.all.find(params[:id])
+    # binding.pry
+  erb :show
 end
